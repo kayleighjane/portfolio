8c9e7a50ff11fe017a0696df6d69974e6ad29b5b
kayleighjaneregan <kayleighjaneregan@gmail.com>
2023-10-29 11:06:32 +0000

    Add link to Github

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -4,7 +4,7 @@ import GithubIcon from './icons/GithubIcon';
 
 const Socials = () => <div className="flex justify-end basis-1/2 gap-x-4">
   <span className="icon-light">
-    <GithubIcon />
+    <a href="https://github.com/kayleighjane/portfolio"><GithubIcon /></a>
   </span>
   <span className="icon-light">
     <LinkedinIcon />
